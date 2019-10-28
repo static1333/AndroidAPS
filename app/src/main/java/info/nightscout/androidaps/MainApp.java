c2e08961443c4ed0262ae6aab153ccb284a47723
jhaydraude <jhaydraude@salesforce.com>
2019-10-28 09:41:33 -0400

    Fix DEV Release and Duplicate Care Portal

--- app/src/main/java/info/nightscout/androidaps/MainApp.java
+++ app/src/main/java/info/nightscout/androidaps/MainApp.java
@@ -185,7 +185,6 @@ public class MainApp extends Application {
             if (Config.PUMPDRIVERS) pluginsList.add(MedtronicPumpPlugin.getPlugin());
             if (!Config.NSCLIENT) pluginsList.add(MDIPlugin.getPlugin());
             pluginsList.add(VirtualPumpPlugin.getPlugin());
-            pluginsList.add(CareportalPlugin.getPlugin());
             if (Config.APS) pluginsList.add(LoopPlugin.getPlugin());
             if (Config.APS) pluginsList.add(OpenAPSMAPlugin.getPlugin());
             if (Config.APS) pluginsList.add(OpenAPSAMAPlugin.getPlugin());
